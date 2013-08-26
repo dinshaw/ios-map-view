e7459fe6f039b6eea876c15e91fe627349762281
Dinshaw Gobhai <gobhai@gmail.com>
2013-08-26 16:39:35 -0400

    map

--- app/controllers/alphabet_controller.rb
+++ app/controllers/alphabet_controller.rb
@@ -1,15 +1,21 @@
 class AlphabetController < UIViewController
+  include MapKit
   def viewDidLoad
     super
     self.title = "Alphabet"
     @table = UITableView.alloc.initWithFrame(CGRectMake(0, 100, view.frame.size.width, view.frame.size.height - 100))
-    @map_view = MKMapView.alloc.initWithFrame(self.view.bounds)
-    @map_view.delegate = self
-    self.view.addSubview @map_view
-    self.view.addSubview @table
     @table.dataSource = self
     @table.backgroundColor = UIColor.clearColor
     @table.delegate = self
+
+    @map_view = MapView.new
+    @map_view.delegate = self
+    @map_view.frame = self.view.frame
+    @map_view.shows_user_location = true
+    @map_view.zoom_enabled = true
+    @map_view.region = CoordinateRegion.new([56, 10.6], [3.1, 3.1])
+    self.view.addSubview @map_view
+    self.view.addSubview @table
     @cells = []
     load_data
   end
@@ -20,18 +26,14 @@ class AlphabetController < UIViewController
     if scroll_offset < 0
       map_frame.origin.y = -100 - (scroll_offset / 3)
     else
+      # @table.origin.y = @table.origin.y - scroll_offset
       map_frame.origin.y = -100 - scroll_offset
     end
     @map_view.frame = map_frame
   end
 
-  def didUpdateUserLocation(user_location, animated=true)
-    # @map_view.setCenterCoordinate(user_location.location.coordinate, true)
-  end
-
   def tableView(tableView, numberOfRowsInSection: section)
-    # return @cells.count + 1
-    20
+    return @cells.count
   end
 
   def tableView(tableView, willDisplayCell: cell, forRowAtIndexPath: indexPath)
